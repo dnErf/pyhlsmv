df6a8e38a0bb4b96156f86aeb220849036a6b360
f <frend.fernando@live.com>
2026-03-24 10:13:32 -0700

    up tailwind daisy

--- src/wui/astro.config.mjs
+++ src/wui/astro.config.mjs
@@ -5,9 +5,15 @@ import vue from '@astrojs/vue';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
   adapter: cloudflare(),
-  output: "server"
+  output: "server",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
---
+++ src/wui/src/components/sidebar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <header>
+            LH
+        </header>
+        <div class="divider"></div>
+        <div>
+            content
+        </div>
+    </div>
+</template>
