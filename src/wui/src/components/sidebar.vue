7f55d03463762bee12c752eadf165b58c959efe5
f <frend.fernando@live.com>
2026-03-25 09:14:55 -0700

    up base page

--- src/wui/src/components/sidebar.vue
+++ src/wui/src/components/sidebar.vue
@@ -4,12 +4,29 @@
 
 <template>
     <div>
-        <header>
-            LH
+        <header class="font-semibold p-4 text-xl">
+            <a href="/">
+                LH
+            </a>
         </header>
         <div class="divider"></div>
-        <div>
-            content
+        <div class="flex flex-col gap-4 px-4 py-1">
+            <div class="flex items-center">
+                <span class="px-2 text-2xl icon-[bx--bar-chart-square]"></span>
+                <span class="px-2">
+                    <a href="/">
+                        Dashboard
+                    </a>
+                </span>
+            </div>
+            <div class="flex items-center">
+                <span class="px-2 text-2xl icon-[bx--book-content]"></span>
+                <span class="px-2">
+                    <a href="/catalog">
+                        Catalog
+                    </a>
+                </span>
+            </div>
         </div>
     </div>
 </template>
